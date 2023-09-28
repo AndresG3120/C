1e0b409fc82d399fb426afbd55d2021c742ea211
Andres Garcia Lopez <andresga@c3r6s2.42malaga.com>
2023-09-28 13:00:28 +0200

    ft_strim

--- cursus/libft/ft_strtrim.c
+++ cursus/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: andresga <andresga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 12:19:33 by andresga          #+#    #+#             */
-/*   Updated: 2023/09/25 13:38:24 by andresga         ###   ########.fr       */
+/*   Updated: 2023/09/28 13:00:16 by andresga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*ft_strtrim(char const *s1, char const *set)
     return(ft_substr(s1, 0, c + 1));
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	a[] = {"paco_paco"};
 	char    b[] = {"p"};
 	printf("%s ", ft_strtrim(a, b));
-}
+}*/
