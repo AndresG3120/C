efadd8aa85b5de7a07f9dfbc5f4f31188e631170
Andres Garcia Lopez <andresga@c3r6s2.42malaga.com>
2023-09-28 14:09:13 +0200

    makefile

--- cursus/libft/ft_substr.c
+++ cursus/libft/ft_substr.c
@@ -6,32 +6,32 @@
 /*   By: andresga <andresga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 11:53:08 by andresga          #+#    #+#             */
-/*   Updated: 2023/09/25 13:30:42 by andresga         ###   ########.fr       */
+/*   Updated: 2023/09/28 13:06:57 by andresga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *p;
-    size_t c;
+	char	*p;
+	size_t	c;
 
-    c = 0;
-    p = malloc(sizeof(char) * (len + 1));
-    if (!p)
-        return NULL;
-    while (c <= len)
-    {
-        p[c] = s[c + start];
-        c++;
-    }
-    p[c] = '\0';
-    return (p);
+	c = 0;
+	p = malloc(sizeof(char) * (len + 1));
+	if (!p)
+		return (NULL);
+	while (c <= len)
+	{
+		p[c] = s[c + start];
+		c++;
+	}
+	p[c] = '\0';
+	return (p);
 }
 
 /*int main(void)
 {
-    char s1[] = {"Hola buenas"};
-    printf("%s", ft_substr(s1,2,2));
+	char s1[] = {"Hola buenas"};
+	printf("%s", ft_substr(s1,2,2));
 }*/
--- cursus/libft/libft.h
+++ cursus/libft/libft.h
@@ -6,19 +6,42 @@
 /*   By: andresga <andresga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:51:29 by andresga          #+#    #+#             */
-/*   Updated: 2023/09/25 13:37:45 by andresga         ###   ########.fr       */
+/*   Updated: 2023/09/28 13:17:34 by andresga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-int	ft_strlen(const char *str);
-char *ft_strchr(const char *s, int c);
-char *ft_substr(char const *s, unsigned int start, size_t len);
+int				ft_atoi(char *str);
+void			ft_bzero(void *s, size_t len);
+void			*ft_calloc(size_t count, size_t size);
+int				ft_isalnum(char *str);
+int				ft_isalpha(char *str);
+int				ft_isascii(char *str);
+int				ft_isdigit(char *str);
+int				ft_isprint(char *str);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memset(void *b, int c, size_t len);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(char *src);
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlcat(char *dest, char *src, size_t dstsize);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+int				ft_strlen(const char *str);
+int				ft_strncmp(char *s1, char *s2, unsigned int n);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+
 #endif
